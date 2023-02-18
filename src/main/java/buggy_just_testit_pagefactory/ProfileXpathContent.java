ed69fa184a0b87f3c89462b42b2034960930bb8c
Devdun <devdun90@gmail.com>
2023-02-18 10:22:37 +0530

    Xpath content

---
+++ src/main/java/buggy_just_testit_pagefactory/ProfileXpathContent.java
@@ -0,0 +1,15 @@
+package buggy_just_testit_pagefactory;
+
+public class ProfileXpathContent {
+    public static final String F_NAME = "firstName";
+    public static final String L_NAME = "lastName";
+    public static final String GENDER = "gender";
+    public static final String AGE = "age";
+    public static final String ADDRESS = "address";
+    public static final String PHONE = "phone";
+    public static final String HOBBY = "hobby";
+    public static final String CURRENT_PWD = "currentPassword";
+    public static final String NEW_PWD = "newPassword";
+    public static final String NEW_PWD_CONFIRM = "newPasswordConfirmation";
+    public static final String SAVE_BTN = "//button[@type='submit']";
+}
---
+++ src/main/java/buggy_just_testit_pagefactory/RegistrationPageXpathContent.java
@@ -0,0 +1,11 @@
+package buggy_just_testit_pagefactory;
+
+public class RegistrationPageXpathContent {
+    public static final String LOGIN_INPUT_FIELD = "username";
+    public static final String F_NAME_INPUT_FIELD = "username";
+    public static final String L_NAME_INPUT_FIELD = "username";
+    public static final String PASSWORD_INPUT_FIELD = "username";
+    public static final String CONFIRM_PWD_INPUT_FIELD = "username";
+    public static final String REGISTER_BTN = "//button[@type='submit'][not(@disabled)][contains(text(),'Register')]";
+    public static final String CANCEL_BTN = "//a[@role='button'][contains(text(),'Cancel')]";
+}
